dcb7d82c34ad1006d2113cf18eb8f21bdc203117
anantranjanjha <anantranjanjha>
2021-11-29 23:39:05 +0530

    setup formatting for amount and date

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
 
 export const Item = ({ description, amount, createdAt, id }) => (
     <div>
         <Link to={`/edit/${description}`} >
             <h1>{description}</h1>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+        {numeral(amount/100).format('$0,0.00')}
+        -
+        {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
     </div>
 );
 
